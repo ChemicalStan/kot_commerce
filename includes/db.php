538e734aee9beb836e486db3f8d687d3a9dd1f39
ChemicalStan <chemicalstan15@gmail.com>
2018-07-04 11:50:10 +0100

    Added admin, includes, and modified links

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php 
+
+try{
+$name = 'king_ot';
+$username = 'root';
+$password = '';
+$host = 'localhost';
+$dsn = "mysql:host=$host;dbname=$name";
+$pdo = new PDO($dsn,$username,$password);
+echo 'We are connected';
+
+}
+catch(PDOException $e){
+    die("Could not connect to the database " . $e->getMessage());
+}
